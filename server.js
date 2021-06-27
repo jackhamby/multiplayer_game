088575c2a0c1a70678f19ae9853c3598797868d8
JackHamby <jackhamby@JackHambys-MBP.zyxel.com>
2021-06-26 22:45:15 -0500

    serve files

--- server.js
+++ server.js
@@ -1,43 +1,109 @@
 const WebSockets = require("ws");
+const uuid = require("uuid");
 
-const webSocket = new WebSockets.Server({
-    port: 3000,
+const express = require('express')
+const app = express()
+const port = 3004
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/client.html');
+});
+  
+app.listen(port, () => {
+    console.log(`game app listening at http://localhost:${port}`)
 });
 
-const connections = [];
+const webSocket = new WebSockets.Server({
+    port: 3005,
+});
 
+const connections = {
+
+}
 const gameState = {
     
 }
 
+const broadcastUpdate = (playerId) => {
+    Object.keys(gameState).forEach((socketId) => {
+        if (playerId === socketId) return;
+        connections[socketId].send(JSON.stringify({
+            type: "UPDATE",
+            x: gameState[playerId].x,
+            y: gameState[playerId].y,
+            playerId,
+        }));
+    });
+}
+
+const broadcastDisconnect = (playerId) => {
+    delete gameState[playerId];
+    delete connections[playerId];
+    Object.keys(gameState).forEach((socketId) => {
+        connections[socketId].send(JSON.stringify({
+            type: "OPPONENT_DISCONNECTED",
+            playerId,
+        }));
+    });
+}
+
 
 webSocket.on("connection", (socket, req) => {
-    console.log('received a connection');
-    const playerNumber = connections.length;
-    // Tell other connections new player
-    connections.forEach((sock) => {
-        sock.send("another player connected");
-    })
-    // Add to connection list
-    connections.push(socket)
+    const playerId = uuid.v4();
 
-    // Update gamestate with new client
-    gameState[playerNumber] = {
-        x: playerNumber === 0 ? 120 : 260, // 0th player at x: 120, 1st player at x: 260
-        y: 360  
-    }
+    connections[playerId] = socket;
+    gameState[playerId] = {
+        x:  Math.floor(Math.random() * 500),
+        y: Math.floor(Math.random() * 500),
+    };
 
-    // Tell client which player they are
+    // Give the connected player their id
     socket.send(JSON.stringify({
-        player: playerNumber,
+        type: "CONNECTED",
+        playerId,
+        gameState
     }));
 
-    // socket.on("message", (message) => {
-    //     console.log(`recieved message: ${message}`);
-    // });
+    // Tell other clients about the new connection
+    Object.keys(connections).forEach((socketId) => {
+        if (playerId == socketId) return;
+        connections[socketId].send(JSON.stringify({
+            type: "OPPONENT_CONNECTED",
+            playerId,
+            gameState, 
+        }));
+    });
 
-    // socket.on("close", (code, reason) => {
+    socket.on('close', (code, playerId) => {
+        console.log(`closed ${code} ${playerId}`);
+        let disconnectedPlayerId;
+        Object.keys(connections).forEach((playerId) => {
+            if (connections[playerId] === socket){
+                disconnectedPlayerId = playerId;
+                broadcastDisconnect(disconnectedPlayerId);
+            }
+        });
 
-    // });
-    console.log(gameState)
+        if (disconnectedPlayerId){
+            delete connections[disconnectedPlayerId];
+        }
+    });
+
+    socket.on("message", (data) => {
+        const message = JSON.parse(data);
+        switch(message.type){
+            case("UPDATE"):
+                console.log(`received a update ${data}`);
+                gameState[message.playerId].x = message.x;
+                gameState[message.playerId].y = message.y;
+                broadcastUpdate(message.playerId);
+                break;
+            case("TEST"):
+                console.log("HIT THIS")
+                break;
+        }
+
+    });
+
+    console.log(`received a connection, there are ${Object.keys(connections).length} connections`);
 });
